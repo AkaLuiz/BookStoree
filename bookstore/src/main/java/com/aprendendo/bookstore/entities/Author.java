5a63913309773580a2e60fbf9c6ef7af9f732266
AkaLuiz <luizcarlosmdea@gmail.com>
2023-08-05 18:03:15 -0300

    Second commit - Entities

---
+++ bookstore/src/main/java/com/aprendendo/bookstore/entities/Author.java
@@ -0,0 +1,30 @@
+package com.aprendendo.bookstore.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+}
---
+++ bookstore/src/main/java/com/aprendendo/bookstore/entities/Book.java
@@ -0,0 +1,51 @@
+package com.aprendendo.bookstore.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int pages;
+
+    @Column(nullable = false)
+    private int chapters;
+
+    @Column(nullable = false)
+    private String ISBN;
+
+    @Column(name = "publisher_name",nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public void Author(){
+
+    }
+
+}
